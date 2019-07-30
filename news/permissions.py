3198e8044d7c43764b042fd31ebfe72b189c1147
Denis Kibet <kibet1816@gmail.com>
2019-07-30 09:36:30 +0300

    Add permissions.py file

---
+++ news/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import SAFE_METHODS,BasePermission
+
+class AdminOnly(BasePermission):
+    def has_permission(self,request,view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
